709e7b4e3c2984945ee46f9f267766f2626ce9fa
Sean Gillespie <seagill@yahoo-inc.com>
2015-05-21 16:52:31 -0700

    Added query method and improved table structure

--- metafs/metafs.py
+++ metafs/metafs.py
@@ -16,9 +16,8 @@ import os
 import sys
 
 # TODO Check structure of db and reset if necessary
-# TODO Store PE Headers
 # TODO ELF/Mach-O Headers
-# TODO anomaly, type, and subsystem id handling
+# TODO anomaly, type, and subsystem handling
 # TODO filemagic for Windows
 # TODO verify proper behavior of section names character sets
 
@@ -61,6 +60,7 @@ class Filer(object):
         self._update_dir_entry(path_hash, path, stat.st_mtime, stat.st_atime, stat.st_ctime)
 
     def _add_file_entry(self, path, filename):
+        file_type = None
         fullpath = os.path.join(path, filename)
         if os.path.isfile(fullpath):
             # Get hash of path
@@ -76,11 +76,12 @@ class Filer(object):
                 except IOError:
                     return
 
+                # If the file has not been seen before get the metadata
                 if not self._check_meta_entry(file_hash):
                     # Default to no headers
                     headers = {}
 
-                    # If the file hasn't been seen before get the magic
+                    # Attempt to get get the magic
                     try:
                         file_type = self.file_magic.from_file(fullpath)
                     except magic.MagicException:
@@ -94,12 +95,12 @@ class Filer(object):
                                 pass
 
                     # insert new file entry
-                    self._insert_meta_entry(file_hash, file_type, stat.st_size, headers)
+                    self._insert_meta_entry(file_hash, headers)
 
                 # This file exists in the filer and only needs updating in the fs map
-                self._update_fs_entry(file_hash, path_hash, filename, stat.st_mtime, stat.st_atime, stat.st_ctime)
+                self._update_file_entry(file_hash, path_hash, filename, file_type, stat.st_size, stat.st_mtime, stat.st_atime, stat.st_ctime)
 
-    def _insert_meta_entry(self, file_hash, file_type, size, headers):
+    def _insert_meta_entry(self, file_hash, headers):
         # Insert file entry into the filer store
         pass
 
@@ -107,8 +108,8 @@ class Filer(object):
         # Insert/Update directory entry in the filer store
         pass
 
-    def _update_fs_entry(self, file_hash, path_hash, filename, mtime, atime, ctime):
-        # Insert/Update fs entry in the filer store
+    def _update_file_entry(self, file_hash, path_hash, filename, file_type, size, mtime, atime, ctime):
+        # Insert/Update file entry in the filer store
         pass
 
     def _check_meta_entry(self, file_hash):
@@ -137,15 +138,14 @@ class SQLiteFiler(Filer):
     def initialize(self, storage):
         conn = sqlite3.connect(storage)
         cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS files
-(file_hash TEXT PRIMARY KEY NOT NULL, magic_hash TEXT NOT NULL, size INT NOT NULL)''')
 
         cursor.execute('''CREATE TABLE IF NOT EXISTS directories
 (path_hash TEXT PRIMARY KEY NOT NULL, path TEXT NOT NULL, mtime INT NOT NULL, ctime INT NOT NULL,
  atime INT NOT NULL)''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS fs
-(file_hash TEXT NOT NULL, path_hash TEXT NOT NULL, filename TEXT KEY NOT NULL, mtime INT NOT NULL,
-ctime INT NOT NULL, atime INT NOT NULL, PRIMARY KEY (file_hash, path_hash))''')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS files
+(file_hash TEXT NOT NULL, path_hash TEXT NOT NULL, filename TEXT KEY NOT NULL, magic_hash KEY TEXT NOT NULL,
+size KEY INT NOT NULL, mtime INT NOT NULL, ctime INT NOT NULL, atime INT NOT NULL,
+PRIMARY KEY (file_hash, path_hash))''')
         cursor.execute('''CREATE TABLE IF NOT EXISTS magic (magic_hash TEXT PRIMARY KEY NOT NULL,
 magic TEXT KEY NOT NULL)''')
         cursor.execute('''CREATE TABLE IF NOT EXISTS peheaders
@@ -186,12 +186,16 @@ v_size INT KEY NOT NULL, entropy REAL KEY NOT NULL)''')
         self.conn.commit()
         self.conn.close()
 
-    def _insert_meta_entry(self, file_hash, file_type, size, headers):
+    def query(self, query):
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        results = cursor.fetchall()
+        cursor.close()
+        return results
+
+    def _insert_meta_entry(self, file_hash, headers):
         # Insert file entry into the filer store
         cursor = self.conn.cursor()
-        magic_hash = hashlib.md5(file_type).hexdigest()
-        cursor.execute("INSERT OR IGNORE INTO files VALUES (?, ?, ?)", (file_hash, magic_hash, size))
-        cursor.execute("INSERT OR IGNORE INTO magic VALUES (?, ?)", (magic_hash, file_type))
         if headers.get("peheaders"):
             if headers["peheaders"].get("exports"):
                 export_dll_name = headers["peheaders"]["exports"].get("dll_name") or ""
@@ -284,10 +288,17 @@ v_size INT KEY NOT NULL, entropy REAL KEY NOT NULL)''')
         cursor.close()
         self.conn.commit()
 
-    def _update_fs_entry(self, file_hash, path_hash, filename, mtime, atime, ctime):
+    def _update_file_entry(self, file_hash, path_hash, filename, file_type, size, mtime, atime, ctime):
         # Insert/Update fs entry in the filer store
         cursor = self.conn.cursor()
-        cursor.execute("REPLACE INTO fs VALUES (?, ?, ?, ?, ?, ?)", (file_hash, path_hash, filename, mtime, atime, ctime))
+        if file_type is not None:
+            magic_hash = hashlib.md5(file_type).hexdigest()
+            cursor.execute("INSERT OR IGNORE INTO magic VALUES (?, ?)", (magic_hash, file_type))
+            cursor.execute("REPLACE INTO files VALUES (?, ?, ?, ?, ?, ?, ? , ?)",
+                           (file_hash, path_hash, filename, magic_hash, size, mtime, atime, ctime))
+        else:
+            cursor.execute("UPDATE files SET mtime=?, atime=?, ctime=? WHERE file_hash=? AND path_hash=?",
+                           (mtime, atime, ctime, file_hash, path_hash))
         cursor.close()
         self.conn.commit()
 
